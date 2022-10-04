6dd5bccd1ec249219cdeddd4275f3c9c9e6a9aeb
TheProtyro <protyro@protonmail.com>
2022-10-04 10:27:43 +0200

    Adding features and updating README

--- README.md
+++ README.md
@@ -8,14 +8,16 @@ Could also be useful to crack some hashes.
 
 * Generate dynamic passwords based on:
 	* The company name (`-c`)
+	* The location (`-l`)
+	* The area code (`-a`)
 	* The country (`-k`)
 	* The bordering countries of the country (from [country_borders.csv](https://github.com/geodatasource/country-borders/))
-	* The location (`-l`)
 	* The year (`-y`), the next year (+1) and the previous years (-3)
+	* Additional key words related to the company (`-w`)
 
-* Add static passwords based on the file `best-passwords.txt`. It is recommended to spray these passwords even if they don't match the password policy requirements. They are added at the end of the wordlist, including the **empty** password.
+* Add static passwords based on the file `best-passwords.txt`. It is recommended to spray these passwords even if they don't match the password policy requirements. They are added at the start of the wordlist, including the **username as password** and the **empty password**.
 
-* Apply the minimum password length from the password policy (`-p`).
+* Apply the minimum password length from the password policy (`-p`). Use `-p 0` for cracking purpose.
 
 * Add special character of your choice at the end of the words (`-s`). You should avoid this option if you use the tool for cracking purpose and use the [OneRuleToRuleThemAll](https://github.com/NotSoSecure/password_cracking_rules/blob/master/OneRuleToRuleThemAll.rule) instead.
 
@@ -38,90 +40,23 @@ The maximal number of attempts possible on 24 hours = (Lockout threshold - 1) * 
 
 ## Usage
 
-	python3 generate_password_spray.py -c Company -k France -l Paris -y 2022 -p 12 -s '!'
-	python3 generate_password_spray.py -c Company -k Luxembourg -l Kirchberg -y 2022 -p 8 
+	# help
+	python3 generate_password_spray.py -h
+
+	# Mandatory arguments
+	python3 generate_password_spray.py -c Company -j Accountant -k Luxembourg -l Kirchberg -a 57 -y 2022 -p 12
+
+	# Optional arguments
+	python3 generate_password_spray.py -c Company -j Accountant -k Luxembourg -l Kirchberg -a 57 -y 2022 -p 12 -s '*' -w 'Excel,Bank,Insurance'
 
 ## Output
 
-
-	# python3 generate_password_spray.py -c Company -k France -l Paris -y 2022 -p 10 -s '!'
-	[+] Generating wordlist...
-	[+] Company_wordlist.txt generated !
-
-	/!\ Don't forget to spray with the username as password, an empty password and the best passwords.
-	 Even if the passwords don't match the password policy.
+	
+	# python3 generate_password_spray.py -c Company -j Accountant -k Luxembourg -l Kirchberg -a 57 -y 2022 -p 12 -s '*' -w 'Excel,Bank,Insurance'
 
 	# cat Company_wordlist.txt
-	Winter2022
-	Spring2022
-	Summer2022
-	Autumn2022
-	Winter2022!
-	Spring2022!
-	Summer2022!
-	Autumn2022!
-	Winter2023
-	Spring2023
-	Summer2023
-	Autumn2023
-	Winter2023!
-	Spring2023!
-	Summer2023!
-	Autumn2023!
-	Winter2021
-	Spring2021
-	Summer2021
-	Autumn2021
-	Winter2021!
-	Spring2021!
-	Summer2021!
-	Autumn2021!
-	Company123
-	Company123!
-	Company2022
-	Company2022!
-	Company2023
-	Company2023!
-	Company2021
-	Company2021!
-	Company2020
-	Company2020!
-	Company2019
-	Company2019!
-	Paris2022!
-	France2022
-	France123!
-	France2022!
-	Andorra123
-	Andorra2022
-	Andorra123!
-	Andorra2022!
-	Belgium123
-	Belgium2022
-	Belgium123!
-	Belgium2022!
-	Switzerland123
-	Switzerland1
-	Switzerland2022
-	Switzerland123!
-	Switzerland1!
-	Switzerland2022!
-	Germany123
-	Germany2022
-	Germany123!
-	Germany2022!
-	Spain2022!
-	Italy2022!
-	Luxembourg123
-	Luxembourg1
-	Luxembourg2022
-	Luxembourg123!
-	Luxembourg1!
-	Luxembourg2022!
-	Monaco2022
-	Monaco123!
-	Monaco2022!
-
+	[empty password]
+	[username as password]
 	P@ssw0rd
 	P@ssw0rd01
 	Password123
@@ -132,3 +67,34 @@ The maximal number of attempts possible on 24 hours = (Lockout threshold - 1) * 
 	Welcome1!
 	Welcome01
 	Welcome01!
+	Company2022*
+	Company2023*
+	Company2021*
+	Company2020*
+	Company2019*
+	Accountant123
+	Accountant2022
+	Accountant57
+	Accountant123*
+	Accountant1*
+	Accountant2022*
+	Accountant57*
+	Kirchberg123
+	Kirchberg2022
+	Kirchberg123*
+	Kirchberg2022*
+	Kirchberg57*
+	Insurance123
+	Insurance2022
+	Insurance123*
+	Insurance2022*
+	Insurance57*
+	Luxembourg123
+	Luxembourg2022
+	Luxembourg57
+	Luxembourg123*
+	Luxembourg1*
+	Luxembourg2022*
+	Luxembourg57*
+	Belgium2022*
+	Germany2022*
--- generate_password_spray.py
+++ generate_password_spray.py
@@ -9,19 +9,25 @@ import csv
 
 parser = argparse.ArgumentParser(description="Password spray tool")
 parser.add_argument("-c", "--company-name", help="Target company", type=str, required=True)
+parser.add_argument("-j", "--job", help="Main job in the company", type=str, required=True)
 parser.add_argument("-k", "--country", help="Target country", type=str, required=True)
 parser.add_argument("-l", "--location", help="Target location", type=str, required=True)
+parser.add_argument("-a", "--area-code", help="Target area code", type=str, required=True)
 parser.add_argument("-y", "--year", help="Current year", type=str, required=True)
 parser.add_argument("-p", "--password-min-length", help="Password minimum length", type=int, required=True)
 parser.add_argument("-s", "--special-char", help="Special char, e.g. '!' or '*'", type=str)
+parser.add_argument("-w", "--words", help="List of key words : word1,word2,word3", type=str)
 args = parser.parse_args()
 
 company = args.company_name
+job = args.job
 country = args.country
 location = args.location
+area_code = args.area_code
 year = args.year
 min_length = args.password_min_length
 special_char = args.special_char
+words = args.words.split(',')
 
 # List of +1 and -3 years based on user input
 years = [year]
@@ -56,9 +62,11 @@ def generate_company():
 	with open(company + '.txt', 'a') as file:
 		file.write(company + '123' + '\n')
 		file.write(company + '1' + '\n')
+		file.write(company + area_code + '\n')
 		if special_char:
 			file.write(company + '123' + special_char + '\n')
 			file.write(company + '1' + special_char + '\n')
+			file.write(company + area_code + special_char + '\n')
 		for year in years:
 			file.write(company + year + '\n')
 			if special_char:
@@ -71,10 +79,12 @@ def generate_country():
 		file.write(country + '123' + '\n')
 		file.write(country + '1' + '\n')
 		file.write(country + year + '\n')
+		file.write(country + area_code + '\n')
 		if special_char:
 			file.write(country + '123' + special_char + '\n')
 			file.write(country + '1' + special_char + '\n')
 			file.write(country + year + special_char + '\n')
+			file.write(country + area_code + special_char + '\n')
 
 		# Generate passwords based on bordering countries
 		# CSV from : https://github.com/geodatasource/country-borders
@@ -99,33 +109,70 @@ def generate_location():
 		file.write(location + '123' + '\n')
 		file.write(location + '1' + '\n')
 		file.write(location + year + '\n')
+		file.write(location + area_code + '\n')
 		if special_char:
 			file.write(location + '123' + special_char + '\n')
 			file.write(location + '1' + special_char + '\n')
 			file.write(location + year + special_char + '\n')
+			file.write(location + area_code + special_char + '\n')
+
+# Generate job related passwords
+def generate_job():
+	with open(company + '.txt', 'a') as file:
+		file.write(job + '123' + '\n')
+		file.write(job + '1' + '\n')
+		file.write(job + year + '\n')
+		file.write(job + area_code + '\n')
+		if special_char:
+			file.write(job + '123' + special_char + '\n')
+			file.write(job + '1' + special_char + '\n')
+			file.write(job + year + special_char + '\n')
+			file.write(job + area_code + special_char + '\n')
+
+# Generate key words related passwords
+def generate_words():
+	if words:
+		with open(company + '.txt', 'a') as file:
+			for word in words:
+				file.write(word + '123' + '\n')
+				file.write(word + '1' + '\n')
+				file.write(word + year + '\n')
+				file.write(word + area_code + '\n')
+				if special_char:
+					file.write(word + '123' + special_char + '\n')
+					file.write(word + '1' + special_char + '\n')
+					file.write(word + year + special_char + '\n')
+					file.write(word + area_code + special_char + '\n')
 
 # Apply the password policy
 def apply_password_min_length():
-	with open(company + '.txt', 'r') as filein, open(company + '_wordlist.txt', 'w') as fileout:
+	with open('best_passwords.txt','r') as filebp, open(company + '.txt', 'r') as filein, open(company + '_wordlist.txt', 'w') as fileout:
+		best_passwords = filebp.readlines()
 		for line in filein:
+			# best_passwords exception
+			if line in best_passwords:
+				fileout.write(line)
 			if len(line) > min_length:
 				fileout.write(line)
 	os.remove(company + '.txt')
 
-# Add the first "static" passwords based on the file best_passwords.txt
+# Add the "static" passwords based on the file best_passwords.txt
 def generate_best_passwords():
-	with open('best_passwords.txt','r') as filein, open(company + '_wordlist.txt', 'a') as fileout:
+	with open('best_passwords.txt','r') as filein, open(company + '.txt', 'a') as fileout:
+		fileout.write('[empty password]\n')
+		fileout.write('[username as password]\n')
 		for password in filein:
 			fileout.write(password)
 
-
 if __name__ == "__main__":
 	print("[+] Generating wordlist...")
-	generate_seasons()
+	generate_best_passwords()
 	generate_company()
+	generate_job()
 	generate_location()
+	generate_words()
+	generate_seasons()
 	generate_country()
 	apply_password_min_length()
-	generate_best_passwords()
 	print("[+] " + company + "_wordlist.txt generated !\n")
 	print("/!\ Don't forget to spray with the username as password, an empty password and the best passwords.\n Even if the passwords don't match the password policy.") 
